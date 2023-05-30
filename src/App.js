a02ba572e1388608786886cb955347c68410f630
vaishnv-sn <vaishnav.sn4@gmail.com>
2023-05-30 14:19:35 +0530

    Banner and App.css completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import NavBar from './components/NavBar/NavBar';
 import './App.css'
+import Banner from './components/Banner/Banner';
 
 function App() {
   return (
+  <>
     <NavBar/>
+    <Banner/>
+  </>
   )
 }
 
