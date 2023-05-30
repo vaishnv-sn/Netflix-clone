81cd883a342f6c8be03418273397c81d2b0aad14
vaishnv-sn <vaishnav.sn4@gmail.com>
2023-05-30 15:44:34 +0530

    Movie posters completed, also designing and component dividing complete

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react'
 import NavBar from './components/NavBar/NavBar';
 import './App.css'
 import Banner from './components/Banner/Banner';
+import RowPost from './components/RowPost/RowPost';
 
 function App() {
   return (
   <>
     <NavBar/>
     <Banner/>
+    <RowPost/>
   </>
   )
 }
 
 export default App;
+
+
+//1994c17e27b12983dc900a374c3e302b
